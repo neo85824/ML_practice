f11c48f0a053aa675dcc4d485703753fbec7987b
neo85824 <neo85824@gmail.com>
2018-07-14 00:39:47 +0800

    GD,SGD,SGDwithADAGRAD

--- Hw1/Hw1.py
+++ Hw1/Hw1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def FetchData(hours):
@@ -20,29 +21,84 @@ def FetchData(hours):
 			Y = np.concatenate( (Y,yi), axis=0)
 	
 	X = np.concatenate( (X, np.ones([X.shape[0],1])), axis=1) #add one 
-	print(X)
 	return X,Y
 
-def GD(X, Y, lr, iter):
+def GD(X, Y, lr, iter): #1e-7
+	ls_rec  = []
+	iter_rec = []
 	theta = np.zeros([X.shape[1],1])
-	Loss = np.sqrt(np.sum((Y-np.dot(X,theta))**2)/X.shape[0])
-	print(Loss)
 	for i in range(iter):
 		gd_theta = -2* np.dot( X.transpose(), (Y-np.dot(X,theta)) ) #-2*X^T(Y-X*theta)
 		theta = theta - lr * gd_theta
-	return theta
+		
+		Loss = np.sqrt(np.sum((Y-np.dot(X,theta))**2)/X.shape[0])
+		ls_rec.append(Loss)
+		iter_rec.append(i)
+	return theta ,ls_rec,iter_rec
+
+
+def SGD_ADAGRAD(X, Y, lr, iter): #1e-4
+	ls_rec  = []
+	iter_rec = []
+	theta = np.zeros([X.shape[1],1])
 	
+	for i in range(iter):
+		n_theta = 0
+		for j in range(X.shape[0]):
+			gd_theta = -2* X[j].transpose() * (Y[j]-np.dot(X[j],theta))
+			gd_theta = gd_theta.reshape(gd_theta.shape[0],1)
+			n_theta = n_theta + gd_theta**2
+			theta = theta - lr/np.sqrt(n_theta) * gd_theta
 			
+		Loss = np.sqrt(np.sum((Y-np.dot(X,theta))**2)/X.shape[0])
+		ls_rec.append(Loss)
+		iter_rec.append(i)
+	return theta,ls_rec,iter_rec
+
+def SGD(X, Y, lr, iter): #1e-7
+	ls_rec  = []
+	iter_rec = []
+	theta = np.zeros([X.shape[1],1])
+	
+	for i in range(iter):
+		n_theta = 0
+		for j in range(X.shape[0]):
+			gd_theta = -2* X[j].transpose() * (Y[j]-np.dot(X[j],theta))
+			gd_theta = gd_theta.reshape(gd_theta.shape[0],1)
+			theta = theta - lr* gd_theta
+			
+		Loss = np.sqrt(np.sum((Y-np.dot(X,theta))**2)/X.shape[0])
+		ls_rec.append(Loss)
+		iter_rec.append(i)
+	return theta,ls_rec,iter_rec
+
+
+
 def main():
 	X,Y = FetchData(9)
-	
-	theta = GD(X, Y, 1e-7, 1000)
-	Loss = np.sqrt(np.sum((Y-np.dot(X,theta))**2)/X.shape[0])
+	theta, ls_rec, iter_rec = SGD(X, Y, 1e-9, 10000)
 	
 	result = np.dot(X,theta)
 	cmp = np.concatenate((Y,result),axis=1)
 	print(cmp)
-	print(Loss)	
+	print(ls_rec[-1])	
+	
+	plt.plot(iter_rec, ls_rec)
+	plt.xlabel("iter")
+	plt.ylabel("Loss")
+	plt.savefig("SGD.png")
+	
+	theta, ls_rec, iter_rec = SGD_ADAGRAD(X, Y, 1e-5, 10000)
+	
+	result = np.dot(X,theta)
+	cmp = np.concatenate((Y,result),axis=1)
+	print(cmp)
+	print(ls_rec[-1])	
+	
+	plt.plot(iter_rec, ls_rec)
+	plt.xlabel("iter")
+	plt.ylabel("Loss")
+	plt.savefig("SGD_ADAGRAD.png")
 	
 	
 	
