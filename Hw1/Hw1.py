86b7881870b3733f2905819f931bee7725f1861d
neo85824 <neo85824@gmail.com>
2018-07-13 23:26:14 +0800

    1st

--- Hw1/Hw1.py
+++ Hw1/Hw1.py
@@ -16,15 +16,36 @@ def FetchData(hours):
 				break
 			xi = pm[j:j+hours].reshape(1,9)
 			yi = np.array([pm[j+hours]]).reshape(1,1)
-			
-		
 			X = np.concatenate( (X,xi), axis=0)
 			Y = np.concatenate( (Y,yi), axis=0)
+	
+	X = np.concatenate( (X, np.ones([X.shape[0],1])), axis=1) #add one 
 	print(X)
-	print(Y)
+	return X,Y
+
+def GD(X, Y, lr, iter):
+	theta = np.zeros([X.shape[1],1])
+	Loss = np.sqrt(np.sum((Y-np.dot(X,theta))**2)/X.shape[0])
+	print(Loss)
+	for i in range(iter):
+		gd_theta = -2* np.dot( X.transpose(), (Y-np.dot(X,theta)) ) #-2*X^T(Y-X*theta)
+		theta = theta - lr * gd_theta
+	return theta
+	
 			
 def main():
-	FetchData(9)
+	X,Y = FetchData(9)
+	
+	theta = GD(X, Y, 1e-7, 1000)
+	Loss = np.sqrt(np.sum((Y-np.dot(X,theta))**2)/X.shape[0])
+	
+	result = np.dot(X,theta)
+	cmp = np.concatenate((Y,result),axis=1)
+	print(cmp)
+	print(Loss)	
+	
+	
+	
 	
 if __name__ == "__main__":
 	main()
